efc2b6d4b181686fce2e8064c45491c1d94d9478
IPa <ipapamagic@gmail.com>
2013-04-16 13:21:55 +0800

    initial commit

---
+++ IPaImageTool/UIImage+IPaImageTool.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+IPaImageTool.h
+//  IPaImageTool
+//
+//  Created by IPaPa on 13/4/16.
+//  Copyright (c) 2013年 IPaPa. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+@interface UIImage (IPaImageTool)
+-(UIImage*)imageWithRotationFix;
+-(UIImage*)imageFitSize:(CGSize)size;
+
+@end
---
+++ IPaImageTool/UIImage+IPaImageTool.m
@@ -0,0 +1,111 @@
+//
+//  UIImage+IPaImageTool.m
+//  IPaImageTool
+//
+//  Created by IPaPa on 13/4/16.
+//  Copyright (c) 2013年 IPaPa. All rights reserved.
+//
+
+#import "UIImage+IPaImageTool.h"
+
+@implementation UIImage (IPaImageTool)
+-(UIImage*)imageWithRotationFix
+{
+    
+    if (self.imageOrientation == UIImageOrientationUp) {
+        return self;
+    }
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, self.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    
+    switch (self.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
+                                             CGImageGetBitsPerComponent(self.CGImage), 0,
+                                             CGImageGetColorSpace(self.CGImage),
+                                             CGImageGetBitmapInfo(self.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (self.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            // Grr...
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage);
+            break;
+    }
+    
+    // And now we just create a new UIImage from the drawing context
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+-(UIImage*)imageFitSize:(CGSize)size
+{
+    CGSize newSize = self.size;
+    CGFloat ratio = self.size.height / self.size.width;
+    if (newSize.width > size.width) {
+        newSize.width = size.width;
+        newSize.height = newSize.width * ratio;
+    }
+    if(newSize.height > size.height) {
+        newSize.height = size.height;
+        newSize.width = newSize.height / ratio;
+    }
+    
+    UIImage *image;
+    UIGraphicsBeginImageContext(newSize);
+    [self drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+@end
