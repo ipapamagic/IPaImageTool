4bfcd157aab2ca6b28e9ef51093942a9d1ddda8f
IPa Chen <ipapamagic@gmail.com>
2020-09-15 11:47:31 +0800

    add convenience initializer hex string to color

--- IPaImageTool/Classes/UIColor+IPaImageTool.swift
+++ IPaImageTool/Classes/UIColor+IPaImageTool.swift
@@ -8,6 +8,35 @@
 import UIKit
 
 extension UIColor {
+    public convenience init(hex: String) {
+        var r, g, b, a: CGFloat
+        r = 0
+        g = 0
+        b = 0
+        a = 0
+        var hexColor = hex
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            hexColor = String(hex[start...])
+        }
+        let scanner = Scanner(string: hexColor)
+        var hexNumber: UInt64 = 0
+        if scanner.scanHexInt64(&hexNumber) {
+            if hexColor.count == 8 {
+                r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                a = CGFloat(hexNumber & 0x000000ff) / 255
+            }
+            else if hexColor.count == 6 {
+                r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                b = CGFloat((hexNumber & 0x0000ff)) / 255
+                a = 1
+            }
+        }
+        self.init(red: r, green: g, blue: b, alpha: a)
+    }
     public var uiImage:UIImage {
         return UIImage.createImage(CGSize(width: 1, height: 1)) { (context) in
                 context.setFillColor(self.cgColor)
