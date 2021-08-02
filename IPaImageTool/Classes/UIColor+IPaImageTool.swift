056afabeb49850869023eb2341832715aceda0c5
IPa Chen <ipapamagic@gmail.com>
2021-08-02 18:23:21 +0800

    implement convenience initializer with int color

--- IPaImageTool/Classes/UIColor+IPaImageTool.swift
+++ IPaImageTool/Classes/UIColor+IPaImageTool.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 extension UIColor {
+    public convenience init(red:Int,green:Int,blue:Int,alpha:Int) {
+        self.init(red: CGFloat(red) / 255.0,  green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: CGFloat(alpha) / 255.0)
+    }
     public convenience init(hex: String) {
         var r, g, b, a: CGFloat
         r = 0
