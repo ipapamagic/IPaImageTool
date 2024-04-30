e942a1cbbd51f9a1edefd32af0cf3572109b22a6
IPaPa <ipapamagic@gmail.com>
2024-04-30 15:27:30 +0800

    recreate qrcode , remove interpolationQuality

--- Sources/IPaImageTool/UIImage+QRCode.swift
+++ Sources/IPaImageTool/UIImage+QRCode.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIImage {
-    public static func qrCodeImage(with string:String,inputCorrectionLevel:String = "M",encoding:String.Encoding = .utf8) -> UIImage? {
+    public static func qrCodeImage(with string:String,size:CGSize = CGSize(width: 100, height: 100),inputCorrectionLevel:String = "M",encoding:String.Encoding = .utf8) -> UIImage? {
         guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
             return nil
         }
@@ -16,6 +16,14 @@ extension UIImage {
         filter.setDefaults()
         filter.setValue(data, forKey: "inputMessage")
         filter.setValue(inputCorrectionLevel, forKey: "inputCorrectionLevel")
-        return filter.outputImage?.uiImage
+        guard let image = filter.outputImage?.uiImage else {
+            return nil
+        }
+        return UIImage.createImage(size) { context in
+            context.interpolationQuality = .none
+            image.draw(in: CGRect(origin: .zero, size: size))
+        }
+        
+        
     }
 }
